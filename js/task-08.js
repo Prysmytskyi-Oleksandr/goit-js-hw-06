b49d210c5a024c794f6dd758696085ade8b8b346
Prysmytskyi-Oleksandr <san2003@ukr.net>
2022-11-07 00:31:25 +0200

    homework revision ver. 2

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,30 @@
 
 const formEl = document.querySelector('.login-form');
+const result = {};
 
 const onSubmit = (event) => {
     event.preventDefault();
     
-const { elements: { email, password } } = event.currentTarget;
+    const { elements: { email, password } } = event.currentTarget;
     if (email.value === "" || password.value === "") {
-       return alert( 'Всі поля мають бути заповнені');
+        return alert('Всі поля мають бути заповнені');
+           
     }
-
+   
+    result.email = email.value;
+    result.password = password.value;
+    console.log(result);
     
-    console.log(`{email: ${email.value} password: ${password.value}}`);
+    //     const formData = new FormData(event.currentTarget);
+    //      console.log(formData);
+    //      formData.forEach((email, password) => {        
+    //       console.log(password);
+    //       console.log(email);
+    // })
 
+     
     formEl.reset();
 }
 
 formEl.addEventListener('submit', onSubmit);
+
